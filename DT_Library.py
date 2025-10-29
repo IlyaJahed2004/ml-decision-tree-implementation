a81857a3195edd0a2eac0a4dcbead49d5b5a9ccf
ilya jahed <ilya.j.h.d@gmail.com>
2025-10-29 16:18:33 +0330

    Handling inconsistencies in the code and refactoring create_tree method

--- DT_Library.py
+++ DT_Library.py
@@ -28,80 +28,80 @@ class DecisionTree():
         self.root = None
 
     #For assigning the Root to the real root node in this tree:
-    def Create_Tree(self,X,Y):
-        Y = Y.map({"satisfied": 1, "dissatisfied": 0, 1:1, 0:0})
-        Y = Y.to_numpy()
-        self.root = self._create_Tree(X,Y,depth= 0)
+    def fit(self, X_Train, Y_Train):
+        if isinstance(Y_Train, pd.Series):
+            Y_Train = Y_Train.map({"satisfied": 1, "dissatisfied": 0, 1:1, 0:0})
+            Y_Train = Y_Train.to_numpy()        
+        self.root = self._create_Tree(X_Train,Y_Train,depth= 0)
         return self.root
     
 
 
     # Y is the label column and X is the dataframe of the attributes
     def _create_Tree(self, X, Y,parent= None ,depth=0):
+
+        # helper method for edge cases in recursion:(like the given method named :_calculate_Value)
+        def plurality_value(Y):  #Y is actually a numpy array
+            yes_counts = (Y == 1).sum()
+            total_counts = len(Y)
+            #1 is interpreted as satisfied and 0 as dissatisfied"
+            return {1:yes_counts/total_counts , 0: (total_counts-yes_counts)/total_counts} #returning a dict of two probability.
+            
+
         all_features =list(X.columns)
         num_Samples = len(Y)
 
 
-        # helper method for edge cases in recursion:
-        def plurality_value(Y):  #Y is actually a numpy array
-            yes_counts = (Y == 1).sum()
-            total_counts = len(Y)
-            return {"Yes":yes_counts/total_counts , "No": (total_counts-yes_counts)/total_counts} #returning a dict of two probability.
-        
-
         # Check stopping conditions (Pre-Pruning)
         if num_Samples >= self.min_Samples and depth < self.max_Depth:
 
-            childtrack= ""
             best_Feature_node_children = []
 
             if(len(all_features)==0):
                 result = plurality_value(Y)
                 probable_label = max(result, key=result.get)
                 leaf_node = Node(isleaf= True,datasample_labels= Y,parent= parent,answer= probable_label)
-
                 return leaf_node
-            elif(X.empty()):
+            
+            elif(X.shape[0] == 0): #when there are no data in the df.
                 # parent_node: i manipulated the node class in a way that it containes the samples too in itself so i can do plurality on that.
                 result = plurality_value(parent.datasample_labels)
                 probable_label = max(result , key=result.get)
-                node = Node(isleaf=True, datasample_labels=Y ,parent=parent,answer=probable_label)
-                return node
+                leaf_node = Node(isleaf=True, datasample_labels=Y ,parent=parent,answer=probable_label)
+                return leaf_node
             
             else:
+                
                 best_Feature_node = self._get_best_Feature(X, Y)   #best-feature here is a node containing the label and the dataframe of the remained attributes.
-
-                if((Y=="satisfied").all()):
+                best_Feature_node.parent = parent
+                if np.all(Y == 1):
                     best_Feature_node.is_leaf =True
-                    best_Feature_node.answer = "satisfied"
-                    best_Feature_node.edge_value = childtrack
-                    best_Feature_node.parent = parent
+                    best_Feature_node.answer = 1      #1 is interpreted as "satisfied"
                     return best_Feature_node
                 
 
-                elif((Y=="dissatisfied").all()):
+                elif np.all(Y==0):
                     best_Feature_node.is_leaf =True
-                    best_Feature_node.answer = "dissatisfied"
-                    best_Feature_node.edge_value = childtrack
-                    best_Feature_node.parent = parent
+                    best_Feature_node.answer = 0    #0 is interpreted as "dissatisfied"
                     return best_Feature_node
 
 
-            # Check gain or gini!
-            for child in best_Feature_node.children:
+            for unique_val in best_Feature_node.children:
                 # TODO: Recursively create child nodes
                 # the child here is only one unique value of the values we have for the selected attribute
-                remained_feature_columns = X.drop(best_Feature_node.feature,axis=1)
-                merged = pd.concat([remained_feature_columns,Y],axis=1)
-                filtered_by_child = merged[merged[best_Feature_node.feature]==child]  #this is a selection by row.
-                new_x = filtered_by_child.iloc[:,:-1]
-                new_y = filtered_by_child.iloc[:,-1]
-                childtrack = child
+                featurename= best_Feature_node.feature
+                local_Y = pd.Series(Y,name='target')
+                merged = pd.concat([X,local_Y],axis=1)
+                filtered_by_child = merged[merged[featurename]==unique_val]  #this is a selection by row.
+                # new_x = remaining features only
+                new_x = filtered_by_child.drop(columns=[featurename, 'target'])
+                # new_y = label column
+                new_y = filtered_by_child['target']
                 # Recursively calling create_tree for the new df and corresponding labels:
                 childnode = self._create_Tree(X= new_x, Y= new_y,parent= best_Feature_node,depth= depth+1)
 
                 #when we reach here it means that we havent faced edge cases:
-                childnode.edge_value = child
+                childnode.edge_value = unique_val
 
                 best_Feature_node_children.append(childnode)
             best_Feature_node.children = best_Feature_node_children
@@ -112,25 +112,29 @@ class DecisionTree():
         else:
             result = plurality_value(Y)
             probable_label = max(result, key=result.get)
-            return Node(isleaf=True, answer=probable_label, datasample_labels=Y)
+            return Node(isleaf=True, answer=probable_label, datasample_labels=Y,parent=parent)
 
 
 
     # X is dataframe of features,Y is series of labels.
     def _get_best_Feature(self, X, Y): 
-        datasample_labels = None
-        # Ensuring that X is a dataframe and Y is a serie
+        # Ensure X is a dataframe
         if isinstance(X, np.ndarray):
             X = pd.DataFrame(X)
-        if isinstance(Y, np.ndarray):
-            Y = pd.Series(Y)
+        # Ensure Y is a pandas Series
+        if isinstance(Y, pd.Series):
+            local_Y = Y.copy()
+        elif isinstance(Y, np.ndarray):
+            local_Y = pd.Series(Y)
+        else:
+            local_Y = pd.Series(Y)  # fallback
 
         if self.mode == "gain":
            # TODO: Calculate information gain for each feature
             max_val = float('-inf')
             best_feature = ""
             for feature in list(X.columns):
-                info_gain = self._information_Gain(feature, X, Y)
+                info_gain = self._information_Gain(feature, X, local_Y)
                 if info_gain > max_val:
                     max_val = info_gain
                     best_feature = feature
@@ -138,12 +142,12 @@ class DecisionTree():
 
         elif self.mode == "gini":
            # TODO: Calculate gini split for each feature
-            max_val = float('-inf')
+            min_val = float('+inf')
             best_feature = ""
             for feature in list(X.columns):
-                info_gain = self._gini_Split(feature, X, Y)
-                if info_gain > max_val:
-                    max_val = info_gain
+                gini = self._gini_Split(feature, X, local_Y)
+                if gini<min_val:
+                    min_val = gini
                     best_feature = feature
         
 
@@ -151,25 +155,25 @@ class DecisionTree():
         # TODO: Select and return best feature as Node
         # for now i put list of names of the children features no a node if required:
         bestfeature_serie = X[best_feature]
-        merged_with_label = pd.concat([bestfeature_serie, Y], axis=1)
+        merged_with_label = pd.concat([bestfeature_serie, local_Y], axis=1)
         children_list = []
         for uniqueval in merged_with_label[best_feature].unique().tolist():
             children_list.append(uniqueval)
-        return Node(feature=best_feature,datasample_labels=Y,children=children_list)
+        return Node(feature=best_feature,datasample_labels=Y,children=children_list) 
     
 
     # infogain is defined for a feature.
     # feature is given to the method as string.(It is the name of the feature)
     def _information_Gain(self, feature, X, Y): #X is the dataframe with all the feature columns.    #Y is a series of the label (ir numpy array when test are running.)
 
-        if (isinstance(Y,pd.Series) and (Y.name is None)):
-            Y = Y.rename("target") 
+        if ((isinstance(Y,pd.Series)) and (Y.name is None)):
+            Y = Y.rename("target")
 
-        # Make sure Y is converted to numpy array just for optimization cases:
-
-        if not isinstance(Y, np.ndarray): # if it is not instance of numpy array we know it is a pandas serie
-            Y = Y.map({"satisfied": 1, "dissatisfied": 0, 1:1, 0:0})
-            Y = Y.to_numpy()
+        if isinstance(Y,np.ndarray):
+            Y_np_local = Y.copy()
+        elif not isinstance(Y, np.ndarray):
+            Y_np_local = Y.map({"satisfied": 1, "dissatisfied": 0})
+            Y_np_local = Y.to_numpy() 
 
        
         def entropy(y):   #in this version of code y is a numpy array of labels passed with the values "0" and "1"
@@ -206,9 +210,11 @@ class DecisionTree():
         if ((isinstance(Y,pd.Series)) and (Y.name is None)):
             Y = Y.rename("target")
 
-        if not isinstance(Y, np.ndarray):
-            Y = Y.map({"satisfied": 1, "dissatisfied": 0})
-            Y = Y.to_numpy()     
+        if isinstance(Y,np.ndarray):
+            Y_np_local = Y.copy()
+        elif not isinstance(Y, np.ndarray):
+            Y_np_local = Y.map({"satisfied": 1, "dissatisfied": 0})
+            Y_np_local = Y.to_numpy()     
 
 
         def gini(y):  #y is an nparray passed to gini method
@@ -224,7 +230,7 @@ class DecisionTree():
 
         desired_column = X[feature]   #we get a serie here.
         desired_nparray = desired_column.to_numpy()  #converted the serie to a nparray.
-        merged = np.column_stack((desired_nparray,Y))  #we have a 2-dimenstional array.the subarrays first element is for features values and the second element is for corresponding label.
+        merged = np.column_stack((desired_nparray,Y_np_local))  #we have a 2-dimenstional array.the subarrays first element is for features values and the second element is for corresponding label.
         
 
         # Sum of the weighted gini's for sub_dataframes which is a rowly filtered version of main dataframe given as input
@@ -239,12 +245,13 @@ class DecisionTree():
         # TODO: Return gini split value
         return Sub_gini_sum
 
+
+
     def _calculate_Value(self, Y):
         # Where is it used and what does it do?
         return max(set(Y), key=list(Y).count)
 
-    def fit(self, X_Train, Y_Train):
-        self.root = self._create_Tree(X_Train, Y_Train)
+
 
     def predict(self, X):
         def _move_Tree(sample, root):
